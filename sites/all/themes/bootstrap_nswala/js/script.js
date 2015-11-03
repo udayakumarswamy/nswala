2bbe9ab8b217b0d8b947a7d5bc0e02450f8848bd
uday <uday.swamy@popcornapps.com>
2015-11-03 17:01:42 +0530

    Modules updated

---
+++ sites/all/themes/bootstrap_nswala/js/script.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+	// add bootstrap classes
+	$('.region-footer').addClass('container');
+	$('footer .block-copyright-block').addClass('col-lg-3 col-md-3 col-sm-3 col-xs-12');
+	$('footer .block-menu').addClass('col-lg-6 col-md-6 col-sm-6 col-xs-12 footer_left');
+	$('footer .block-widgets').addClass('col-lg-3 col-md-3 col-sm-3 col-xs-12 footer_right');
+});

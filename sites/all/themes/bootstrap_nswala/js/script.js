85a1ed42d3dab0c4ca90e5073985ceb81365e1d5
udaya kumar swamy vasa <udayakumarswamy@gmail.com>
2015-11-03 22:25:16 +0530

    Home page styles updated

--- sites/all/themes/bootstrap_nswala/js/script.js
+++ sites/all/themes/bootstrap_nswala/js/script.js
@@ -1,5 +1,16 @@
 jQuery(document).ready(function($) {
 	// add bootstrap classes
+	$('.navbar').removeClass('container');
+	$('.navbar-header').addClass('col-lg-3 col-md-3 col-sm-3 col-xs-12 no-pad');
+	$('.navbar-collapse').addClass('col-lg-9 col-md-9 col-sm-9 col-xs-12 text-right pull-right');
+
+
+	$('.pane-home-page-my-announcements .views-row').addClass('col-lg-6 col-md-6 col-sm-6 col-xs-12 announce_descp no-pad');
+	$('.pane-home-page-my-announcements .views-row-first').removeClass('col-lg-6 col-md-6 col-sm-6 col-xs-12 announce_descp no-pad');
+	$('.pane-home-page-my-announcements .views-row-first').addClass('col-lg-12 col-md-12 col-sm-12 col-xs-12 announce_descp no-pad');
+	$('.pane-home-page-my-announcements .views-row img').addClass('img-responsive');
+
+
 	$('.region-footer').addClass('container');
 	$('footer .block-copyright-block').addClass('col-lg-3 col-md-3 col-sm-3 col-xs-12');
 	$('footer .block-menu').addClass('col-lg-6 col-md-6 col-sm-6 col-xs-12 footer_left');
